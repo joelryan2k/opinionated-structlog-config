7b50e475aaf428265d99e584f5a30bb7c73f2f13
Joel Ryan <joel@iwcenter.com>
2024-07-17 14:48:18 -0400

    fix: Django email logging is in the default config

--- opinionated_structlog_config/django.py
+++ opinionated_structlog_config/django.py
@@ -7,15 +7,6 @@ def configure_django_for_structlog(middleware: list[str], config: typing.Union[d
 
     common_configure_structlog(config or {})
 
-    loggers = {}
-
-    if config == None or 'SENTRY' not in config:
-        loggers['django.request'] = {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
-        }
-
     return {
         "version": 1,
         "disable_existing_loggers": False,
@@ -23,20 +14,11 @@ def configure_django_for_structlog(middleware: list[str], config: typing.Union[d
             "console_formatter": build_formatter()
         },
         "handlers": {
-            "mail_admins": {
-                "level": "ERROR",
-                "filters": ["require_debug_false"],
-                "class": "django.utils.log.AdminEmailHandler",
-            },
             "console": {
                 "level": "DEBUG",
                 "formatter": "console_formatter",
                 "class": "logging.StreamHandler",
             },
-        },
-        "loggers": loggers,
-        "filters": {
-            "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}
         },
         "root": {
             "handlers": ["console"],
